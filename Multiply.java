bf08908eb088691ab8c5cb4907a38d0b9d92ba19
Pandurang Choudhari <cpandurang4@gmail.com>
2022-05-19 19:12:09 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+public class Sample{
+
+	public static void main (String[]args){
+
+		int a = 21;
+		int b = 22;
+
+		System.out.println("The multiplication of the above numbers is " + (a*b));
+
+	}
+}
